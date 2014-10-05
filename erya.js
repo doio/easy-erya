7ce875a5fb041188eecc423889e7703f356130bf
buchenglei <myopen@yeah.net>
2014-10-05 11:07:38 +0800

    修复一些小问题

--- erya.js
+++ erya.js
@@ -1,7 +1,7 @@
 //判断当前页面是否加载了jQery库，防止重复加载造成冲突
 if(typeof(jQuery)=="undefined"){
-	var my_jquery=document.createElement('script');
-	my_jquery.src='http://libs.baidu.com/jquery/2.0.0/jquery.min.js';
+	var my_jquery=document.createElement("script");
+	my_jquery.src="http://libs.baidu.com/jquery/2.0.0/jquery.min.js";
 	document.body.appendChild(my_jquery);
 }
 switch(pageWhere()){
@@ -9,11 +9,11 @@ switch(pageWhere()){
 		freeVideo();
 		break;
 	default:
-		alert("嗯，在这个页面好像没什么可做！");
+		alert("嗯，在这个页面好像没什么可做!");
 }
 
 function pageWhere(){
-	if($("span:contains('火狐浏览器')")>0){
+	if($("span:contains('火狐浏览器')").length>0){
 		return "video";
 	}
 	return "nothing";
